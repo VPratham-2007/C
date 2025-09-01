0dfcaee8b56acc541bfbff40ec32641c5ac90851
VPratham@2007 <vaghelapratham2007@gmail.com>
2025-09-01 20:14:01 +0530

    Added Array and Structure_Strings folders

--- Array/array_using_pointers.c
+++ Array/array_using_pointers.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int main (){
+int main()
+{
 
-    int marks[] = {78,53,45,35};
-    int* ptr = marks; // Same as (int* ptr = &marks[0];)
+    int marks[] = {78, 53, 45, 35};
+    int *ptr = marks; // Same as (int* ptr = &marks[0];)
 
     for (int i = 0; i <= 3; i++)
     {
-        printf ("The address of marks %d is stored at %u\n",marks[i],ptr);
+        printf("The address of marks %d is stored at %u\n", marks[i], ptr);
         ptr++;
     }
-    
+
     return 0;
 }
