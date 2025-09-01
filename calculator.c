8f2ca84a18f23b54682540a9b470c5702352f1c3
VPratham@2007 <vaghelapratham2007@gmail.com>
2025-09-02 00:01:34 +0530

    Added Calculator

--- calculator.c
+++ calculator.c
@@ -1,50 +1,50 @@
-#include <stdio.h>  
+#include <stdio.h>
 
-int main (){
+int main()
+{
 
-    float a[10],b[10];
+    float a[10], b[10];
     int no;
     char operation;
-    printf ("The no of operations you want to do:\n");
-    scanf ("%d",&no);
-    printf ("Enter the first digit:\n");
-    scanf ("%f",&a[0]);
+    printf("The no of operations you want to do:\n");
+    scanf("%d", &no);
+    printf("Enter the first digit:\n");
+    scanf("%f", &a[0]);
 
     for (int i = 0; i < no; i++)
     {
-        printf ("Enter another digit\n");
-        scanf ("%f",&b[i]);
-        printf ("Enter 'A' for Addition\nEnter 'S' for Subtraction\nEnter 'M' for Multiplication\nEnter 'D' for Division\n");
-        scanf (" %c",&operation);
+        printf("Enter another digit\n");
+        scanf("%f", &b[i]);
+        printf("Enter 'A' for Addition\nEnter 'S' for Subtraction\nEnter 'M' for Multiplication\nEnter 'D' for Division\n");
+        scanf(" %c", &operation);
 
         if (operation == 'A')
         {
-            a[i+1]=a[i]+b[i];
+            a[i + 1] = a[i] + b[i];
         }
-        
+
         else if (operation == 'S')
         {
-            a[i+1]=a[i]-b[i];   
+            a[i + 1] = a[i] - b[i];
         }
 
         else if (operation == 'M')
         {
-            a[i+1]=a[i]*b[i];   
+            a[i + 1] = a[i] * b[i];
         }
 
         else if (operation == 'D')
         {
-            a[i+1]=a[i]/b[i];   
-        }
-        
-        else
-        {
-            a[i+1]=a[i];
+            a[i + 1] = a[i] / b[i];
         }
 
+        else
+        {
+            a[i + 1] = a[i];
+        }
     }
-    
-    printf ("The final ans is %.2f\n",a[no]);
+
+    printf("The final ans is %.2f\n", a[no]);
 
     return 0;
 }
